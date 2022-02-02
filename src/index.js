681ba801498af0652309fb6ebc2e67c2adc79bf3
unknown <ermessonlimadossantos@gmail.com>
2022-02-02 02:00:30 -0300

    feat: start FinApi

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customersAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customersAlreadyExists) {
+    return response.status(400).json({ error: "Usúario já cadastrado!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/account", (request, response) => {
+    return response.json(customers)
+})
+
+app.listen(3333);
